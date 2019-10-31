bed28ac35c62b2faa80c6a9aebe515a5ff2b7238
Hasan Abdullah <hasan.cse91@gmail.com>
2019-10-31 11:20:12 +0600

    androidx migration, single table project

--- 01.SQLite-Single-Table-CRUD/app/src/main/java/com/hellohasan/sqlite_project/Features/ShowStudentList/CustomViewHolder.java
+++ 01.SQLite-Single-Table-CRUD/app/src/main/java/com/hellohasan/sqlite_project/Features/ShowStudentList/CustomViewHolder.java
@@ -1,6 +1,6 @@
 package com.hellohasan.sqlite_project.Features.ShowStudentList;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
--- 01.SQLite-Single-Table-CRUD/app/src/main/java/com/hellohasan/sqlite_project/Features/ShowStudentList/StudentListRecyclerViewAdapter.java
+++ 01.SQLite-Single-Table-CRUD/app/src/main/java/com/hellohasan/sqlite_project/Features/ShowStudentList/StudentListRecyclerViewAdapter.java
@@ -2,8 +2,8 @@ package com.hellohasan.sqlite_project.Features.ShowStudentList;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
